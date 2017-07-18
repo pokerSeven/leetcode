4647d516992aef6bb8f69b305e56259e076024e4
pokerSeven <cxlhjwow@163.com>
2017-07-19 00:53:29 +0800

    add

--- 12.py
+++ 12.py
@@ -6,3 +6,24 @@ Given an integer, convert it to a roman numeral.
 Input is guaranteed to be within the range from 1 to 3999.
 
 """
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        c = [["","I","II","III","IV","V","VI","VII","VIII","IX"],
+             ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+             ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+             ["", "M", "MM", "MMM"]]
+        roman = ""
+        roman += c[3][(num / 1000) % 10]
+        roman += c[2][(num / 100) % 10]
+        roman += c[1][(num / 10) % 10]
+        roman += c[0][num  % 10]
+        return roman
+
+s  = Solution()
+print s.intToRoman(1984)
+
+
